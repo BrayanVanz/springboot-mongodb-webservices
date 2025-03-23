5a8b0dac82995d30c45f03b681302375557ff010
Brayan Vanz de Oliveira <brayanvanzdeoliveira@gmail.com>
2025-03-23 16:43:09 -0300

    Get Post by id

---
+++ src/main/java/com/brayanvanz/nosqlwebservices/services/PostService.java
@@ -0,0 +1,22 @@
+package com.brayanvanz.nosqlwebservices.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.brayanvanz.nosqlwebservices.domain.Post;
+import com.brayanvanz.nosqlwebservices.repositories.PostRepository;
+import com.brayanvanz.nosqlwebservices.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> post = repository.findById(id);
+        return post.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+}
