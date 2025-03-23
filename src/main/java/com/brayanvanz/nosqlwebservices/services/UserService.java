bd6eb27b530a4c85df9762720ef9f55e8bbf5909
Brayan Vanz de Oliveira <brayanvanzdeoliveira@gmail.com>
2025-03-23 06:25:32 -0300

    Search user by Id

--- src/main/java/com/brayanvanz/nosqlwebservices/services/UserService.java
+++ src/main/java/com/brayanvanz/nosqlwebservices/services/UserService.java
@@ -1,12 +1,14 @@
 package com.brayanvanz.nosqlwebservices.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.brayanvanz.nosqlwebservices.domain.User;
 import com.brayanvanz.nosqlwebservices.repositories.UserRepository;
+import com.brayanvanz.nosqlwebservices.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = repository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 }
---
+++ src/main/java/com/brayanvanz/nosqlwebservices/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,8 @@
+package com.brayanvanz.nosqlwebservices.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+}
