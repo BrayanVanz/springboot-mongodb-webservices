eee751335b222e110810106948e2754b4d9f19fc
Brayan Vanz de Oliveira <brayanvanzdeoliveira@gmail.com>
2025-03-23 04:23:50 -0300

    First Commit

---
+++ src/main/java/com/brayanvanz/nosqlwebservices/NosqlwebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.brayanvanz.nosqlwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NosqlwebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NosqlwebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brayanvanz/nosqlwebservices/NosqlwebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brayanvanz.nosqlwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NosqlwebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
