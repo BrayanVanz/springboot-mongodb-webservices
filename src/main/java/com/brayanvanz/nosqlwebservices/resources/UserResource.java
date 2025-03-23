5608d29842540486b68bb3f9f1232a0e078a7518
Brayan Vanz de Oliveira <brayanvanzdeoliveira@gmail.com>
2025-03-23 06:04:04 -0300

    Return users as DTO objects

--- src/main/java/com/brayanvanz/nosqlwebservices/resources/UserResource.java
+++ src/main/java/com/brayanvanz/nosqlwebservices/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.brayanvanz.nosqlwebservices.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.brayanvanz.nosqlwebservices.domain.User;
+import com.brayanvanz.nosqlwebservices.dto.UserDTO;
 import com.brayanvanz.nosqlwebservices.services.UserService;
 
 @RestController
@@ -19,8 +21,9 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> users = service.findAll();
-        return ResponseEntity.ok().body(users);
+        List<UserDTO> usersDTO = users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(usersDTO);
     }
 }
