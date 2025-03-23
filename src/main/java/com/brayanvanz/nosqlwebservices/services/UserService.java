1d66eca3c5faa28ccc09d6a07c71d36701283bba
Brayan Vanz de Oliveira <brayanvanzdeoliveira@gmail.com>
2025-03-23 06:52:45 -0300

    Insert user

--- src/main/java/com/brayanvanz/nosqlwebservices/services/UserService.java
+++ src/main/java/com/brayanvanz/nosqlwebservices/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.brayanvanz.nosqlwebservices.domain.User;
+import com.brayanvanz.nosqlwebservices.dto.UserDTO;
 import com.brayanvanz.nosqlwebservices.repositories.UserRepository;
 import com.brayanvanz.nosqlwebservices.services.exceptions.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> user = repository.findById(id);
         return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User user) {
+        return repository.insert(user);
+    }
+
+    public User fromDTO(UserDTO userDTO) {
+        return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
     }
 }
